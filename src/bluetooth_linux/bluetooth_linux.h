7d0c6b62d1bb6b556c40772e22c913e5acf95319
Esteve Fernandez <esteve.fernandez@gmail.com>
2006-10-02 21:29:55 +0000

    sdp
    git-svn-id: http://ruby-bluetooth.rubyforge.org/svn/trunk@10 587dd09e-be1c-0410-a341-8ca66d93110d

--- src/bluetooth_linux/bluetooth_linux.h
+++ src/bluetooth_linux/bluetooth_linux.h
@@ -10,17 +10,17 @@
 void Init_ruby_bluetooth();
 
 struct bluetooth_device_struct
-  {
+{
     VALUE addr;
     VALUE name;
-  };
+};
 
 struct bluetooth_service_struct
-  {
+{
     VALUE name;
     VALUE description;
     VALUE provider;
-  };
+};
 
 static VALUE bt_device_new(VALUE self, VALUE name, VALUE addr);
 
@@ -43,3 +43,7 @@ static VALUE bt_l2cap_socket_init(int argc, VALUE *argv, VALUE sock);
 static VALUE bt_l2cap_socket_connect(VALUE sock, VALUE host, VALUE port);
 
 static VALUE bt_service_new(VALUE self, VALUE name, VALUE description, VALUE provider);
+
+static VALUE bt_service_register(VALUE self);
+
+static VALUE bt_service_unregister(VALUE self);
